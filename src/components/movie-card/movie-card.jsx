adf7ce980f00bc65829318c667bf598e87ddac17
corto0406 <132603219+corto0406@users.noreply.github.com>
2023-09-13 22:38:18 +0200

    immersion 3.4.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,26 @@
+// Here you import the PropTypes library
+import PropTypes from "prop-types";
+
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
+};
+
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
