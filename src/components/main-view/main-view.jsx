eed53d477051566717df5c68fa9039fa5479d8f9
corto0406 <132603219+corto0406@users.noreply.github.com>
2023-09-16 22:49:31 +0200

    immersion 3.5.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,50 +1,108 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import {SignupView} from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
     const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        fetch("https://movie-place-35ed6ca44a78.herokuapp.com")
-            .then((response) => response.json())
-            .then((data) => {
-                const moviesFromApi = data.docs.map((doc) => {
-                    return {
-                        director: doc.director_name?.[0],
-                        title: doc.title,
-                        description: doc.description,
-                        genre: doc.genre
-                    };
-
-                });
-
-                setMovies(moviesFromApi);
-            });
-    }, []);
-
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+
+    useEffect(() => {
+        if (!token) return;
+
+        fetch("https://movie-place-35ed6ca44a78.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+            .then((response) => response.json())
+            .then((movies) => {
+                setMovies(movies);
+
+            });
+    }, [token]);
+
+
+    if (!user) {
+        return (
+            <LoginView
+                onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }}
+            />
+        );
+    }
+
+    if (!user) {
+        return (
+          <>
+            <LoginView onLoggedIn={(user, token) => {
+              setUser(user);
+              setToken(token);
+            }} />
+            or
+            <SignupView />
+          </>
+        );
+      }
+
 
     if (selectedMovie) {
         return <MovieView movie={selectedMovie} />;
     }
 
+    if (selectedMovie) {
+        return (
+            <>
+                <button
+                    onClick={() => {
+                        setUser(null);
+                    }}
+                >
+                    Logout
+                </button>
+                <MovieView
+                    movie={selectedMovie}
+                    onBackClick={() => setSelectedMovie(null)}
+                />
+            </>
+        );
+    }
+
     if (movies.length === 0) {
-        return <div>The list is empty!</div>;
+        return (
+            <>
+                <button
+                    onClick={() => {
+                        setUser(null);
+                    }}
+                >
+                    Logout
+                </button>
+                <div>The list is empty!</div>
+            </>
+        );
     }
 
     return (
         <div>
-            <button
-                onClick={() => {
-                    alert("Nice!");
-                }}
-            >
-                Click me!
-            </button>
-            {movie.map((movie) => (
-                <MovieCard key={movie.title} movie={movie} />
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+            {movies.map((movie) => (
+                <MovieCard
+                    key={movie.title}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
             ))}
         </div>
     );
-}
+};
+
+
